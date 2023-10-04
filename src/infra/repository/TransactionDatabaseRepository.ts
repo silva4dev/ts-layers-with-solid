00100cbb7a59eaf2df1dd141e5c83439a545cff3
silva4dev <web4dev.lucas@outlook.com>
2023-10-04 20:00:11 -0300

    chore: remove schema in connection query

--- src/infra/repository/TransactionDatabaseRepository.ts
+++ src/infra/repository/TransactionDatabaseRepository.ts
@@ -9,16 +9,16 @@ export default class TransactionDatabaseRepository implements TransactionReposit
 	}
 
 	async save(transaction: Transaction): Promise<void> {
-		await this.connection.query("insert into branas.transaction (code, amount, number_installments, payment_method) values ($1, $2, $3, $4)", [transaction.code, transaction.amount, transaction.numberInstallments, transaction.paymentMethod]);
+		await this.connection.query("insert into transaction (code, amount, number_installments, payment_method) values ($1, $2, $3, $4)", [transaction.code, transaction.amount, transaction.numberInstallments, transaction.paymentMethod]);
 		for (const installment of transaction.installments) {
-			await this.connection.query("insert into branas.installment (code, number, amount) values ($1, $2, $3)", [transaction.code, installment.number, installment.amount]);
+			await this.connection.query("insert into installment (code, number, amount) values ($1, $2, $3)", [transaction.code, installment.number, installment.amount]);
 		}
 	}
 
 	async get(code: string): Promise<Transaction> {
-		const transactionData = await this.connection.one("select * from branas.transaction where code = $1", [code]);
+		const transactionData = await this.connection.one("select * from transaction where code = $1", [code]);
 		const transaction = new Transaction(transactionData.code, parseFloat(transactionData.amount), transactionData.number_installments, transactionData.payment_method);
-		const installmentsData = await this.connection.query("select * from branas.installment where code = $1", [code]);
+		const installmentsData = await this.connection.query("select * from installment where code = $1", [code]);
 		for (const installmentData of installmentsData) {
 			const installment = new Installment(installmentData.number, parseFloat(installmentData.amount));
 			transaction.installments.push(installment);
